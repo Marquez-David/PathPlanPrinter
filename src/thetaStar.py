306d5b7a70dde2e1ceb8de6633b2c0e4c6ef32aa
David Márquez Mínguez <45391647+Marquez-David@users.noreply.github.com>
2020-05-08 16:13:30 +0200

    empienzo a hacer thetaStar
    :+1:

---
+++ src/thetaStar.py
@@ -0,0 +1,14 @@
+def updateVertex(puntoInicial, puntoFinal):
+
+
+def lineOsSight(puntoInicial, puntoFinal):
+    """
+    punto, punto -> boolean
+    PRE:
+    OBJ:
+    """
+    
+    x0 = puntoInicial.x  #coordenada 'x' del punto inicial
+    y0 = puntoInicial.y  #coordenada 'y' del punto inicial
+    x1 = puntoFinal.x    #coordenada 'x' del punto final
+    y1 = puntoFinal.y    #coordenada 'y' del punto final
